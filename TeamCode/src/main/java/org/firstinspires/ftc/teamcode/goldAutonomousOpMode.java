1c15029afc7d1a79b72300f81b08108529cd499c
dphag <darren.hagler@4cacademy.org>
2018-11-01 13:54:21 -0400

    move code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/goldAutonomousOpMode.java
@@ -0,0 +1,337 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name="GoldAutonomous", group="Commodores")
+//@Disabled
+
+public class goldAutonomousOpMode extends LinearOpMode {
+    //Sensors
+    private BNO055IMU imu;
+    private DigitalChannel elevatorExtended;
+
+    //Outputs
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private DcMotor armMotor;
+    private Servo leftElevatorMotor;
+    private Servo rightElevatorMotor;
+    private Servo landerServo;
+    private Servo leftArmServo;
+    private Servo rightArmServo;
+
+    private Orientation lastAngles = new Orientation();
+    double globalAngle, correction;
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;    // REV 20:1 Planetary Gearbox
+    static final double     DRIVE_GEAR_REDUCTION    = 1.5 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 3.54331;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    // These constants define the desired driving/control characteristics
+    // The can/should be tweaked to suite the specific robot drive train.
+    static final double     DRIVE_SPEED             = 0.7;     // Nominal speed for better accuracy.
+    static final double     TURN_SPEED              = 0.5;     // Nominal half speed for better accuracy.
+
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        // setup hardware
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        elevatorExtended = hardwareMap.get(DigitalChannel.class, "elevatorExtended");
+
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        leftElevatorMotor = hardwareMap.get(Servo.class, "leftElevatorMotor");
+        rightElevatorMotor = hardwareMap.get(Servo.class, "rightElevatorMotor");
+        landerServo = hardwareMap.get(Servo.class, "landerServo");
+        leftArmServo = hardwareMap.get(Servo.class,"leftArmServo");
+        rightArmServo = hardwareMap.get(Servo.class,"rightArmServo");
+
+        // setup Motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // brake motors
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // set digital channels to input mode.
+        elevatorExtended.setMode(DigitalChannel.Mode.INPUT);
+
+        // hold robot up
+        landerServo.setPosition(0);
+
+        // ensure basket is retracted
+        leftArmServo.setPosition(0.17);
+        rightArmServo.setPosition(0.2);
+
+        // set gyro parameters
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.mode                = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled      = false;
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "BNO055IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(parameters);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
+        // make sure the imu gyro is calibrated before continuing.
+        while (!isStopRequested() && !imu.isGyroCalibrated())
+        {
+            sleep(50);
+            idle();
+        }
+
+        telemetry.addData("Mode", "waiting for start");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+
+        // wait for start button.
+        waitForStart();
+
+        telemetry.addData("Mode", "running");
+        telemetry.update();
+
+        sleep(1000);
+
+        // drive until end of period.
+        while (opModeIsActive())
+        {
+            telemetry.addData("1 imu heading", lastAngles.firstAngle);
+            telemetry.addData("2 global heading", globalAngle);
+            telemetry.addData("3 correction", correction);
+            telemetry.update();
+
+            // release elevator
+            landerServo.setPosition(0.25);
+            sleep(1000);
+
+            // lower robot
+            leftElevatorMotor.setPosition(0);
+            rightElevatorMotor.setPosition(1.0);
+            sleep(500);
+            leftElevatorMotor.setPosition(0.5);
+            rightElevatorMotor.setPosition(0.5);
+            sleep(1000);
+
+            // rotate to unhook
+            rotate(17, TURN_SPEED);
+
+            // sit pretty
+            while (runtime.seconds() < 30.0)
+            {
+                telemetry.addData("Path", "Complete");
+                telemetry.update();
+            }
+
+
+            //Turn 45 degree angle, get away.
+
+            //rotate(45, TURN_SPEED);
+            //driveGyro(DRIVE_SPEED, 44.0);
+
+            //Turn 90 degree angle and drop marker.
+            //rotate(-90,TURN_SPEED);
+            //driveGyro(DRIVE_SPEED, 28.0);
+            //armMotor.setPower(1);
+            //Drive across cater line.
+            //driveGyro(DRIVE_SPEED, 70);
+        }
+    }
+
+    /**
+     * Resets the cumulative angle tracking to zero.
+     */
+    private void resetAngle()
+    {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        globalAngle = 0;
+    }
+
+    /**
+     * Get current cumulative angle rotation from last reset.
+     * @return Angle in degrees. + = left, - = right.
+     */
+    private double getAngle()
+    {
+        // We experimentally determined the Z axis is the axis we want to use for heading angle.
+        // We have to process the angle because the imu works in euler angles so the Z axis is
+        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
+        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
+
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+
+        globalAngle += deltaAngle;
+
+        lastAngles = angles;
+
+        return globalAngle;
+    }
+
+    /**
+     * See if we are moving in a straight line and if not return a power correction value.
+     * @return Power adjustment, + is adjust left - is adjust right.
+     */
+    private double checkDirection()
+    {
+        // The gain value determines how sensitive the correction is to direction changes.
+        // You will have to experiment with your robot to get small smooth direction changes
+        // to stay on a straight line.
+        double correction, angle, gain = .2;
+
+        angle = getAngle();
+
+        if (angle == 0)
+            correction = 0;             // no adjustment.
+        else
+            correction = -angle;        // reverse sign of angle for correction.
+
+        correction = correction * gain;
+
+        return correction;
+    }
+
+    /**
+     * Rotate left or right the number of degrees. Does not support turning more than 180 degrees.
+     * @param degrees Degrees to turn, + is left - is right
+     */
+    private void rotate(int degrees, double power)
+    {
+        double  leftPower, rightPower;
+
+        // restart imu movement tracking.
+        resetAngle();
+
+        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
+        // clockwise (right).
+
+        if (degrees < 0)
+        {   // turn right.
+            leftPower = power;
+            rightPower = -power;
+        }
+        else if (degrees > 0)
+        {   // turn left.
+            leftPower = -power;
+            rightPower = power;
+        }
+        else return;
+
+        // set power to rotate.
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
+
+        // rotate until turn is completed.
+        if (degrees < 0)
+        {
+            // On right turn we have to get off zero first.
+            while (opModeIsActive() && getAngle() == 0) {}
+
+            while (opModeIsActive() && getAngle() > degrees) {}
+        }
+        else    // left turn.
+            while (opModeIsActive() && getAngle() < degrees) {}
+
+        // turn the motors off.
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
+
+        // wait for rotation to stop.
+        sleep(1000);
+
+        // reset angle tracking on new heading.
+        resetAngle();
+    }
+
+    /**
+     * Method to drive robot in a straight line, based on encoder counts
+     */
+    private void driveGyro(double speed, double distance)
+    {
+        int     newLeftTarget;
+        int     newRightTarget;
+        int     moveCounts;
+
+        // restart imu movement tracking.
+        resetAngle();
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            moveCounts = (int)(distance * COUNTS_PER_INCH);
+            newLeftTarget = leftMotor.getCurrentPosition() + moveCounts;
+            newRightTarget = rightMotor.getCurrentPosition() + moveCounts;
+
+            // Set Target and Turn On RUN_TO_POSITION
+            leftMotor.setTargetPosition(newLeftTarget);
+            rightMotor.setTargetPosition(newRightTarget);
+
+            leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // start motion.
+            speed = Range.clip(Math.abs(speed), 0.0, 1.0);
+            leftMotor.setPower(speed*.5);
+            rightMotor.setPower(speed*.5);
+
+            // keep looping while we are still active, and BOTH motors are running.
+            while (opModeIsActive() && (leftMotor.isBusy() && rightMotor.isBusy())) {
+
+                // Use gyro to drive in a straight line.
+                correction = checkDirection();
+
+                // if driving in reverse, the motor correction also needs to be reversed
+                if (distance < 0)
+                    correction *= -1.0;
+
+                leftMotor.setPower(speed + correction);
+                rightMotor.setPower(speed - correction);
+            }
+
+            // Stop all motion;
+            leftMotor.setPower(0);
+            rightMotor.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleopOpMode.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Teleop", group="Commodores")
+
+public class teleopOpMode extends LinearOpMode {
+    //Sensors
+    private DigitalChannel elevatorExtended;
+
+    //Outputs
+    private DcMotor leftMotor;
+    private DcMotor rightMotor;
+    private DcMotor armMotor;
+    private Servo leftElevatorMotor;
+    private Servo rightElevatorMotor;
+    private Servo landerServo;
+    private Servo leftArmServo;
+    private Servo rightArmServo;
+
+    @Override
+    public void runOpMode() {
+        // setup hardware
+        elevatorExtended = hardwareMap.get(DigitalChannel.class, "elevatorExtended");
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class, "rightMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+        leftElevatorMotor = hardwareMap.get(Servo.class, "leftElevatorMotor");
+        rightElevatorMotor = hardwareMap.get(Servo.class, "rightElevatorMotor");
+        landerServo = hardwareMap.get(Servo.class, "landerServo");
+        leftArmServo = hardwareMap.get(Servo.class,"leftArmServo");
+        rightArmServo = hardwareMap.get(Servo.class,"rightArmServo");
+
+        // setup Motors
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // brake motors
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // set digital channels to input mode.
+        elevatorExtended.setMode(DigitalChannel.Mode.INPUT);
+
+        // hold robot up
+        landerServo.setPosition(0);
+
+        // ensure basket is retracted
+        leftArmServo.setPosition(0.17);
+        rightArmServo.setPosition(0.2);
+
+        // wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // setup speed variables
+        double leftPower = 0;
+        double rightPower = 0;
+        double armPower = 0;
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // driving is from gamepad1 sticks
+            leftPower = -this.gamepad1.left_stick_y;
+            rightPower = -this.gamepad1.right_stick_y;
+            leftMotor.setPower(leftPower);
+            rightMotor.setPower(rightPower);
+
+            // arm is controlled by the gamepad2 triggers
+            armPower = gamepad2.right_trigger * .6;
+            // if the left trigger is pressed, reverse
+            if (gamepad2.left_trigger != 0.0 && gamepad2.right_trigger == 0.0) {
+                armPower = -gamepad2.left_trigger * .6;
+            }
+            armMotor.setPower(armPower);
+
+            //elevator is controlled by gamepad2 bumpers
+            if (gamepad2.right_bumper && !gamepad2.left_bumper && elevatorExtended.getState()) {
+                leftElevatorMotor.setPosition(0);
+                rightElevatorMotor.setPosition(1.0);
+            }
+            else if (gamepad2.left_bumper && !gamepad2.right_bumper) {
+                leftElevatorMotor.setPosition(1.0);
+                rightElevatorMotor.setPosition(0.0);
+            }
+            else {
+                leftElevatorMotor.setPosition(0.5);
+                rightElevatorMotor.setPosition(0.5);
+            }
+
+            // latch for elevator
+            if (gamepad1.right_bumper) {
+                landerServo.setPosition(0);
+            } else {
+                landerServo.setPosition(0.25);
+            }
+
+            // extend the basket
+            if (gamepad2.dpad_up){
+                leftArmServo.setPosition(.8);
+                rightArmServo.setPosition(.8);
+            } else if (gamepad2.dpad_down){
+                leftArmServo.setPosition(0.17);
+                rightArmServo.setPosition(0.2);
+            }
+
+            // view sensor and motor states
+            telemetry.addData("Elevator Extended Limit: ", elevatorExtended.getState());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+}
